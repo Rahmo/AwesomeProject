7e1a711652dd6b911e378f285e281775ce8991d1
Rahmo Alzahrani <Abdulrahman.alzahrani@morningstar.com>
2020-11-29 20:35:52 -0600

    shopping list items added withs states and icons

--- App.js
+++ App.js
@@ -6,16 +6,30 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {StyleSheet, View, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View,Text, StatusBar, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import uuid from 'uuidv4';
 
 const App: () => React$Node = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'milk'},
+    {id: uuid(), text: 'butter'},
+    {id: uuid(), text: 'Juice'},
+    {id: uuid(), text: 'bread'},
+  ]);
+  const renderItem = ({item}) => <ListItem item={item} />;
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
         <Header title="Shopping List" />
+        <FlatList
+          data={items}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </>
   );
